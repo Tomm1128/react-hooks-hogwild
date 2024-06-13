d20aa5430b0ab683ab4f397b5fb00f89141248f6
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-13 11:32:20 -0400

    Create hide functionality for each pig tile

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 
 const HogTile = ({name, image, specialty, weight, greased, medals}) => {
   const [detailsOn, setDetails] = useState(false)
+  const [isHidden, setHidden] = useState(false)
 
   const additionalDetails = (
     <div className="details">
@@ -12,14 +13,26 @@ const HogTile = ({name, image, specialty, weight, greased, medals}) => {
    </div>
   )
 
+  const checkDisplay = {
+     display: isHidden ? "none" : "in-line"
+  }
+
   const handleImageClick = () => {
     setDetails(!detailsOn)
   }
 
+  const updateHidden = () => {
+    setHidden(!isHidden)
+  }
+
 
   return (
-    <div className="pigTile">
-      <h3>{name}</h3>
+    <div className="pigTile" style={checkDisplay}>
+      <div>
+        <h3>{name}</h3>
+        <label>Hide </label>
+        <input type="checkbox" checked={isHidden} onChange={updateHidden}></input>
+      </div>
       <img src={image} alt="Pig" onClick={handleImageClick}></img>
       {detailsOn ? additionalDetails : ""}
     </div>
